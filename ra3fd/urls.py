2b0f83da6892c9c45de9a65aba71f0f83a338672
sk <cww@bk.ru>
2022-05-08 13:24:39 +0300

    mode__iregex --> mode

--- ra3fd/urls.py
+++ ra3fd/urls.py
@@ -1,4 +1,4 @@
-"""ra3fd URL Configuration
+""" ra3fd URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
